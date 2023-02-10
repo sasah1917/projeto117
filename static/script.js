b917e453e7a8e1b0919f79e8c7fb33ab12b64e22
sasah1917 <92746412+sasah1917@users.noreply.github.com>
2023-02-10 14:32:12 -0300

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+
+    console.log('Ready')
+
+    //  Busque a data atual e atualize-a no DOM
+    var date = new Date()
+    let display_date= "Data:" + date.toLocaleDateString()
+    
+    $(document).ready(function () {
+        $("#date").html(display_date)
+        $('#button').prop('disabled', true);
+    })
+    
+    let avaliacoes;
+    // Escreva um evento, quando o botão Enviar for clicado
+    $('#button').click(function(){
+
+        //  Obtenha o valor do texto da área de texto usando o método 'val()'
+        let text_value = $('#text').val()
+
+        //  Converta-o em um objeto JS.
+        //  Forneça uma "chave" aqui e escreva o mesmo no arquivo app.py também para extrair dados
+        let input_text = {'posts' : text_value}
+        console.log(input_text)
+
+        //  requisição ajax
+        $.ajax({
+
+            //  tipo da requisição web
+            type : 'POST',
+
+            //  dados a serem enviados no formato JSON
+            data : JSON.stringify(),
+
+            //  o tipo de resposta esperado é json
+            dataType : 'json',
+
+            //  contentType
+            contentType : 'application/json',
+
+            //  se tudo funcionar, execute esta função
+            success : function(result){
+
+                // extraia previsão e a URL do emoticon do resultado
+               
+            },
+
+
+                //  atualize os elementos DOM
+
+
+                //  exiba-os
+
+            },
+
+            //  se houver algum erro, execute esta função
+            error : function(result){
+
+                console.log(result)
+            }
+        })
+
+
+        //  limpando a caixa de texto após cada pressionamento de botão
+        $('#text').val("")
+    })
+        
+})
